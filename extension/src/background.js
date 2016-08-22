6dfca55c920635a695d4d3380afaff0f7a104718
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2016-08-22 22:46:21 +0200

    Find all visible images from content script and replace them with compressed images

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,37 +1,17 @@
-const skipUrls = [
-    'bandwidth-hero.s3.amazonaws.com',
-    'syndication.twitter.com',
-    'facebook.com/(tr/|rsrc.php|impression.php)',
+const skipPatterns = [
+    'bandwidth-hero\.s3\.amazonaws\.com',
+    'syndication\.twitter\.com',
+    'facebook\.com/(tr/|rsrc\.php|impression\.php)',
     'google-analytics',
     'favicon',
     '.*\.svg'
 ];
-const socketUri = 'wss://lit-inlet-44494.herokuapp.com/';
 const placeholder = chrome.extension.getURL('/res/images/placeholder.png');
-var socket = new WebSocket(socketUri);
-var connected = false;
-
-socket.onopen = () => connected = true;
-socket.onclose = () => connected = false;
-socket.onmessage = rawMessage => {
-    if (rawMessage == 'ping') return;
-
-    const message = JSON.parse(rawMessage.data);
-    setTimeout(() => chrome.tabs.sendMessage(message.tabId, message), 1000);
-};
 
 chrome.webRequest.onBeforeRequest.addListener(
     details => {
-        if (details.url.match(RegExp(`(${skipUrls.join('|')})`, 'i'))) return;
-        if (details.url.match(/https?:\/\/.+/i)) {
-            if (!connected) {
-                socket = new WebSocket(socketUri);
-            }
-            socket.send(JSON.stringify({
-                tabId: details.tabId,
-                url: details.url
-            }));
-
+        if (!details.url.match(RegExp(`(${skipPatterns.join('|')})`, 'i'))
+            && details.url.match(/https?:\/\/.+/i)) {
             return {
                 redirectUrl: placeholder
             };
--- extension/src/content.js
+++ extension/src/content.js
@@ -1,14 +1,74 @@
-chrome.runtime.onMessage.addListener(request => {
-    [...document.getElementsByTagName('img')].forEach(image => {
-        if (image.src == request.original) {
-            image.src = request.compressed;
-        }
-    });
-    [...document.querySelectorAll('div,span')].forEach(block => {
-        var bg = window.getComputedStyle(block).backgroundImage;
-        if (bg.includes(request.original)) {
-            block.style.backgroundImage = `url('${request.compressed}')`;
-        }
-    });
+const socketUri = 'wss://lit-inlet-44494.herokuapp.com/';
+var socket;
+var connected = false;
+var elementsByUrl = {};
+var pendingUrls = [];
+var observer = new MutationObserver(mutations => {
+    mutations.forEach(mutation => mutation.addedNodes.forEach(traverse));
 });
 
+connect(socketUri);
+document.body.childNodes.forEach(traverse);
+observer.observe(document.body, {childList: true, subtree: true});
+
+function connect(socketUri) {
+    socket = new WebSocket(socketUri);
+
+    socket.onopen = () => {
+        connected = true;
+        processPendingUrls();
+    };
+
+    socket.onclose = () => {
+        connected = false;
+        setTimeout(() => connect(socketUri), 1000);
+    };
+
+    socket.onmessage = rawMessage => {
+        const message = JSON.parse(rawMessage.data);
+        if (elementsByUrl[message.original]) {
+            const node = elementsByUrl[message.original];
+            if (node.nodeName == 'IMG') {
+                node.src = message.compressed;
+            } else {
+                node.style.backgroundImage = `url('${message.compressed}')`;
+            }
+        }
+    };
+}
+
+function traverse(node) {
+    if (node.nodeType == Node.ELEMENT_NODE && node.offsetParent !== null) {
+        if (node.nodeName == 'IMG' && node.hasAttribute('src')) {
+            replaceImage(node, node.src);
+        } else {
+            var backgroundImage = window.getComputedStyle(node).backgroundImage;
+            var url = backgroundImage.match(/url\(['"]+(.*)['"]+\)/i);
+            if (url && url.length) {
+                replaceImage(node, url[1]);
+            }
+        }
+
+        // TODO handle iframes
+
+        if (node.hasChildNodes()) {
+            node.childNodes.forEach(traverse);
+        }
+    }
+}
+
+function replaceImage(node, imageUrl) {
+    if (elementsByUrl[imageUrl]) return;
+
+    elementsByUrl[imageUrl] = node;
+    pendingUrls.push(imageUrl);
+    processPendingUrls();
+}
+
+function processPendingUrls() {
+    if (!connected) return;
+
+    while (pendingUrls.length > 0) {
+        socket.send(pendingUrls.pop());
+    }
+}

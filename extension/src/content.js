74baa768a91a62070326359b67f37b2a44956d8f
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2016-08-23 12:35:30 +0200

    Change code style and add docs

--- extension/src/content.js
+++ extension/src/content.js
@@ -1,62 +1,54 @@
-const socketUri   = 'wss://lit-inlet-44494.herokuapp.com/';
+// TODO make server uri configurable
+const serverUri   = 'wss://lit-inlet-44494.herokuapp.com/';
 var socket;
-var connected     = false;
 var elementsByUrl = {};
 var pendingUrls   = [];
 var observer      = new MutationObserver(mutations => {
-    mutations.forEach(mutation => mutation.addedNodes.forEach(traverse));
+    mutations.forEach(mutation => mutation.addedNodes.forEach(processNode));
 });
 
-connect(socketUri);
-document.body.childNodes.forEach(traverse);
+document.body.childNodes.forEach(processNode);
 observer.observe(document.body, {childList: true, subtree: true});
 
-function connect(socketUri) {
-    socket = new WebSocket(socketUri);
-
-    socket.onopen = () => {
-        connected = true;
-        processPendingUrls();
-    };
-
-    socket.onclose = () => connected = false;
-
-    socket.onmessage = rawMessage => {
-        const message = JSON.parse(rawMessage.data);
-        const node    = elementsByUrl[message.original];
-        if (node !== undefined) {
-            if (node.nodeName == 'IMG') {
-                node.src = message.compressed;
-            } else {
-                node.style.backgroundImage = `url('${message.compressed}')`;
-            }
-        }
-    };
-}
-
-function traverse(node) {
+/**
+ * Check if node is a DOM element and is visible.
+ * Try to find image URL in the element and add it to the processing queue.
+ * Traverse all node children recursively.
+ *
+ * @param {Node} node to process
+ */
+function processNode(node) {
     if (node.nodeType == Node.ELEMENT_NODE && node.offsetParent !== null) {
         if (node.nodeName == 'IMG' && node.hasAttribute('src')) {
-            replaceImage(node, node.src);
+            requestImageCompression(node.src, node);
         } else {
             var url = window
                 .getComputedStyle(node)
                 .backgroundImage
                 .match(/url\(['"]+(.*)['"]+\)/i);
             if (url && url.length) {
-                replaceImage(node, url[1]);
+                requestImageCompression(url[1], node);
             }
         }
 
         // TODO handle iframes
 
         if (node.hasChildNodes()) {
-            node.childNodes.forEach(traverse);
+            node.childNodes.forEach(processNode);
         }
     }
 }
 
-function replaceImage(node, imageUrl) {
+/**
+ * Add image URL to the processing queue and
+ * keep reference to the node it belongs to,
+ * so that we can update this node when we receive
+ * compressed image back from server.
+ *
+ * @param {String} imageUrl
+ * @param {Node}   node
+ */
+function requestImageCompression(imageUrl, node) {
     if (elementsByUrl[imageUrl]) return;
 
     elementsByUrl[imageUrl] = node;
@@ -64,13 +56,50 @@ function replaceImage(node, imageUrl) {
     processPendingUrls();
 }
 
+/**
+ * Connect to compressing server if it's not connected yet,
+ * and send all queued image URLs to the server.
+ */
 function processPendingUrls() {
-    if (!connected) {
-        if (socket.readyState !== WebSocket.CONNECTING) connect(socketUri);
+    if (!socket || socket.readyState !== WebSocket.OPEN) {
+        if (!socket || socket.readyState !== WebSocket.CONNECTING) connect();
         return;
     }
 
     while (pendingUrls.length > 0) {
         socket.send(pendingUrls.pop());
     }
+}
+
+/**
+ * Receive response from the server, which should have following format:
+ * {
+ *   "original": "<original_image_url>",
+ *   "compressed": "<compressed_image_url>"
+ * }
+ *
+ * Look up node corresponding to the original image and update it.
+ *
+ * @param {String} response JSON as a string
+ */
+function processResponse(response) {
+    const data = JSON.parse(response.data);
+    const node = elementsByUrl[data.original];
+    if (node !== undefined) {
+        if (node.nodeName == 'IMG') {
+            node.src = data.compressed;
+        } else {
+            node.style.backgroundImage = `url('${data.compressed}')`;
+        }
+    }
+}
+
+/**
+ * Connect to server via WebSocket and assign event handlers.
+ */
+function connect() {
+    socket = new WebSocket(serverUri);
+
+    socket.onopen    = processPendingUrls;
+    socket.onmessage = processResponse;
 }

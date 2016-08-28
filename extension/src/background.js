0501fa71c7293b9b28667048f5f457501e9a0d56
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2016-08-28 19:27:52 +0200

    Add configurable options

--- extension/src/background.js
+++ extension/src/background.js
@@ -11,80 +11,102 @@ const skipPatterns            = [
     '.*\.svg'
 ];
 
-chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest,
-    {
-        urls:  ['<all_urls>'],
-        types: ['image']
-    },
-    ['blocking']
-);
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.storage.sync.set({
+        enabled:   true,
+        serverUrl: 'wss://bandwidth-hero.herokuapp.com/',
+    });
+});
 
-chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived,
-    {
-        urls:  ['<all_urls>'],
-        types: ['main_frame', 'sub_frame']
-    },
-    ['blocking', 'responseHeaders']
-);
+chrome.storage.sync.get(options => {
+    let enabled = options.enabled;
 
-chrome.extension.onMessage.addListener(onMessage);
+    chrome.extension.onMessage.addListener(onMessage);
+    chrome.webRequest.onBeforeRequest.addListener(onBeforeRequest,
+        {
+            urls:  ['<all_urls>'],
+            types: ['image']
+        },
+        ['blocking']
+    );
+    chrome.webRequest.onHeadersReceived.addListener(onHeadersReceived,
+        {
+            urls:  ['<all_urls>'],
+            types: ['main_frame', 'sub_frame']
+        },
+        ['blocking', 'responseHeaders']
+    );
 
-/**
- * Redirect images to default placeholder to prevent full image loading.
- *
- * @param {Object} details
- * @returns {Object} object
- */
-function onBeforeRequest(details) {
-    if (!details.url.match(RegExp(`(${skipPatterns.join('|')})`, 'i'))
-        && details.url.match(/https?:\/\/.+/i)) {
+    /**
+     * Handle incoming messages from extension/popup.
+     *
+     * @param {Object} request
+     */
+    function onMessage(request) {
+        const actionHandlers = {
+            'setActiveIcon':  () => {
+                chrome.browserAction.setIcon({
+                    path: 'res/images/icon-128-active.png'
+                });
+            },
+            'setDefaultIcon': () => {
+                chrome.browserAction.setIcon({
+                    path: 'res/images/icon-128.png'
+                });
+            },
+            'enable':         () => {
+                enabled = true;
+                chrome.browserAction.setIcon({
+                    path: 'res/images/icon-128.png'
+                });
+            },
+            'disable':        () => {
+                enabled = false;
+                chrome.browserAction.setIcon({
+                    path: 'res/images/icon-128-disabled.png'
+                });
+            }
+        }
+
+        if (request.action in actionHandlers) {
+            actionHandlers[request.action]();
+        }
+    }
+
+    /**
+     * Redirect images to default placeholder to prevent full image loading.
+     *
+     * @param {Object} details
+     * @returns {Object} object
+     */
+    function onBeforeRequest(details) {
+        if (enabled
+            && !details.url.match(RegExp(`(${skipPatterns.join('|')})`, 'i'))
+            && details.url.match(/https?:\/\/.+/i)) {
+            return {
+                redirectUrl: placeholder
+            };
+        }
+    }
+
+    /**
+     * Patch Content-Security-Policy header to allow compressed images loading.
+     *
+     * @param {Object} details
+     * @returns {Object} patched headeers
+     */
+    function onHeadersReceived(details) {
+        for (var i = 0; i < details.responseHeaders.length; i++) {
+            if (/content-security-policy/i.test(details.responseHeaders[i].name)) {
+                details.responseHeaders[i].value = details
+                    .responseHeaders[i]
+                    .value
+                    .replace('img-src', `img-src ${compressedImagesBaseUrl}`);
+            }
+        }
+
         return {
-            redirectUrl: placeholder
+            responseHeaders: details.responseHeaders
         };
     }
-}
-
-/**
- * Patch Content-Security-Policy header to allow compressed images loading.
- *
- * @param {Object} details
- * @returns {Object} patched headeers
- */
-function onHeadersReceived(details) {
-    for (var i = 0; i < details.responseHeaders.length; i++) {
-        if (/content-security-policy/i.test(details.responseHeaders[i].name)) {
-            details.responseHeaders[i].value = details
-                .responseHeaders[i]
-                .value
-                .replace('img-src', `img-src ${compressedImagesBaseUrl}`);
-        }
-    }
-
-    return {
-        responseHeaders: details.responseHeaders
-    };
-}
-
-/**
- * Handle incoming messages from extension/popup.
- *
- * @param {Object} request
- */
-function onMessage(request) {
-    const actionHandlers = {
-        'setActiveIcon':  () => {
-            chrome.browserAction.setIcon({
-                path: 'res/images/icon-128-active.png'
-            });
-        },
-        'setDefaultIcon': () => {
-            chrome.browserAction.setIcon({
-                path: 'res/images/icon-128.png'
-            });
-        }
-    }
-
-    if (request.action in actionHandlers) {
-        actionHandlers[request.action]();
-    }
-}
+});
--- extension/src/popup.js
+++ extension/src/popup.js
@@ -1,1 +1,20 @@
 'use strict';
+
+document.addEventListener('DOMContentLoaded', () => {
+    const enabledSwitch = document.getElementById('bh_enabled');
+    const serverUrlInp  = document.getElementById('bh_server_url');
+
+    chrome.storage.sync.get(options => {
+        if (options.enabled && !enabledSwitch.checked) {
+            enabledSwitch.click();
+        }
+        serverUrlInp.value = options.serverUrl;
+    });
+
+    enabledSwitch.addEventListener('change', () => {
+        chrome.storage.sync.set({enabled: enabledSwitch.checked});
+        chrome.extension.sendMessage({
+            action: enabledSwitch.checked ? 'enable' : 'disable'
+        });
+    });
+});

8979be7ef9875b3ff1539b3041936283d9d9bf5d
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2016-09-05 14:31:43 +0200

    Use Standard JS

--- extension/src/background.js
+++ extension/src/background.js
@@ -1,120 +1,120 @@
-'use strict';
+/* global chrome */
+'use strict'
 
 const defaults = {
-    enabled:           true,
-    serverUrl:         'wss://bandwidth-hero.herokuapp.com/',
-    compressedBaseUrl: 'bandwidth-hero.s3.amazonaws.com'
-};
+  enabled: true,
+  serverUrl: 'wss://bandwidth-hero.herokuapp.com/',
+  compressedBaseUrl: 'bandwidth-hero.s3.amazonaws.com'
+}
 
 /**
  * Set default settings when extension is installed.
  */
 chrome.runtime.onInstalled.addListener(() => {
-    chrome.storage.sync.set(defaults);
-});
+  chrome.storage.sync.set(defaults)
+})
 
-chrome.storage.sync.get(runBackground);
+chrome.storage.sync.get(runBackground)
 
-function runBackground(settings) {
-    settings    = Object.assign({}, defaults, settings);
-    let enabled = settings.enabled;
+function runBackground (settings) {
+  settings = Object.assign({}, defaults, settings)
+  let enabled = settings.enabled
 
-    if (!enabled) {
-        chrome.browserAction.setIcon({path: 'res/images/icon-128-disabled.png'});
+  if (!enabled) {
+    chrome.browserAction.setIcon({ path: 'res/images/icon-128-disabled.png' })
+  }
+
+  chrome.extension.onMessage.addListener(handleMessage)
+  chrome.webRequest.onBeforeRequest.addListener(replaceImagesWithPlaceholder,
+    {
+      urls: [ '<all_urls>' ],
+      types: [ 'image' ]
+    },
+    [ 'blocking' ]
+  )
+  chrome.webRequest.onHeadersReceived.addListener(patchContentSecurityPolicy,
+    {
+      urls: [ '<all_urls>' ],
+      types: [ 'main_frame', 'sub_frame' ]
+    },
+    [ 'blocking', 'responseHeaders' ]
+  )
+  return
+
+  /**
+   * Handle incoming messages from extension/popup.
+   *
+   * @param Object request
+   */
+  function handleMessage (request) {
+    const actionHandlers = {
+      'setActiveIcon': () => {
+        chrome.browserAction.setIcon({
+          path: 'res/images/icon-128-active.png'
+        })
+      },
+      'setDefaultIcon': () => {
+        chrome.browserAction.setIcon({
+          path: 'res/images/icon-128.png'
+        })
+      },
+      'enable': () => {
+        enabled = true
+        chrome.browserAction.setIcon({
+          path: 'res/images/icon-128.png'
+        })
+      },
+      'disable': () => {
+        enabled = false
+        chrome.browserAction.setIcon({
+          path: 'res/images/icon-128-disabled.png'
+        })
+      }
     }
 
-    chrome.extension.onMessage.addListener(handleMessage);
-    chrome.webRequest.onBeforeRequest.addListener(replaceImagesWithPlaceholder,
-        {
-            urls:  ['<all_urls>'],
-            types: ['image']
-        },
-        ['blocking']
-    );
-    chrome.webRequest.onHeadersReceived.addListener(patchContentSecurityPolicy,
-        {
-            urls:  ['<all_urls>'],
-            types: ['main_frame', 'sub_frame']
-        },
-        ['blocking', 'responseHeaders']
-    );
-    return;
+    if (request.action in actionHandlers) {
+      actionHandlers[ request.action ]()
+    }
+  }
 
-    /**
-     * Handle incoming messages from extension/popup.
-     *
-     * @param {Object} request
-     */
-    function handleMessage(request) {
-        const actionHandlers = {
-            'setActiveIcon':  () => {
-                chrome.browserAction.setIcon({
-                    path: 'res/images/icon-128-active.png'
-                });
-            },
-            'setDefaultIcon': () => {
-                chrome.browserAction.setIcon({
-                    path: 'res/images/icon-128.png'
-                });
-            },
-            'enable':         () => {
-                enabled = true;
-                chrome.browserAction.setIcon({
-                    path: 'res/images/icon-128.png'
-                });
-            },
-            'disable':        () => {
-                enabled = false;
-                chrome.browserAction.setIcon({
-                    path: 'res/images/icon-128-disabled.png'
-                });
-            }
-        }
+  /**
+   * Redirect images to default placeholder to prevent full image loading.
+   *
+   * @param Object details
+   * @returns Object object
+   */
+  function replaceImagesWithPlaceholder (details) {
+    const allowPatterns = [
+      settings.compressedBaseUrl,
+      'favicon',
+      '.*.svg'
+    ]
+    if (enabled && !details.url.match(RegExp(`(${allowPatterns.join('|')})`, 'i')) &&
+      details.url.match(/https?:\/\/.+/i)) {
+      return {
+        redirectUrl: chrome.extension.getURL('/res/images/placeholder.png')
+      }
+    }
+  }
 
-        if (request.action in actionHandlers) {
-            actionHandlers[request.action]();
-        }
+  /**
+   * Patch Content-Security-Policy header to allow compressed images loading.
+   *
+   * @param Object details
+   * @returns Object patched headers
+   */
+  function patchContentSecurityPolicy (details) {
+    for (let i = 0; i < details.responseHeaders.length; i++) {
+      if (/content-security-policy/i.test(details.responseHeaders[ i ].name)) {
+        details.responseHeaders[ i ].value = details
+          .responseHeaders[ i ]
+          .value
+          .replace('img-src', `img-src ${settings.compressedBaseUrl}`)
+      }
     }
 
-    /**
-     * Redirect images to default placeholder to prevent full image loading.
-     *
-     * @param {Object} details
-     * @returns {Object} object
-     */
-    function replaceImagesWithPlaceholder(details) {
-        const allowPatterns = [
-            settings.compressedBaseUrl,
-            'favicon',
-            '.*\.svg'
-        ];
-        if (enabled
-            && !details.url.match(RegExp(`(${allowPatterns.join('|')})`, 'i'))
-            && details.url.match(/https?:\/\/.+/i)) {
-            return {
-                redirectUrl: chrome.extension.getURL('/res/images/placeholder.png')
-            };
-        }
+    return {
+      responseHeaders: details.responseHeaders
     }
-
-    /**
-     * Patch Content-Security-Policy header to allow compressed images loading.
-     *
-     * @param {Object} details
-     * @returns {Object} patched headeers
-     */
-    function patchContentSecurityPolicy(details) {
-        for (var i = 0; i < details.responseHeaders.length; i++) {
-            if (/content-security-policy/i.test(details.responseHeaders[i].name)) {
-                details.responseHeaders[i].value = details
-                    .responseHeaders[i]
-                    .value
-                    .replace('img-src', `img-src ${settings.compressedBaseUrl}`);
-            }
-        }
-
-        return {
-            responseHeaders: details.responseHeaders
-        };
-    }
+  }
 }

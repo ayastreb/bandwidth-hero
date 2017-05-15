55ec842db6101aef230a0d9142b4a0cdd61d5df1
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-05-15 17:32:58 +0900

    Send transformed image type and length in response headers.

--- webtask/compress-webtask.js
+++ webtask/compress-webtask.js
@@ -8,17 +8,19 @@ module.exports = (context, req, res) => {
   const imageUrl = req.query.url
   if (!imageUrl.match(/^https?:/i)) return res.status(400).end()
 
-  const errorHandler = err => console.log(`Error in ${imageUrl}: ${err}`)
   const transformer = sharp().grayscale().toFormat('webp', {
     quality: JPEG_QUALITY,
     compressionLevel: 9,
     progressive: true
   })
-  transformer.on('error', errorHandler)
+  transformer.on('error', err => console.log(`Error in ${imageUrl}: ${err}`))
+  transformer.on('data', processedImage => {
+    console.log(`Compressed: ${imageUrl}`)
+    res.writeHead(200, {
+      'Content-Type': 'image/webp',
+      'Content-Length': processedImage.length
+    })
+  })
 
-  request(imageUrl)
-    .pipe(transformer)
-    .pipe(res)
-    .on('error', errorHandler)
-    .on('finish', () => console.log(`Compressed: ${imageUrl}`))
+  request(imageUrl).pipe(transformer).pipe(res)
 }

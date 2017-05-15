0dc4bd4d73ccd325b916da60441506d3bd3d5194
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-05-16 00:33:42 +0900

    Update WebP options.

--- webtask/compress-webtask.js
+++ webtask/compress-webtask.js
@@ -2,17 +2,13 @@
 const request = require('request')
 const sharp = require('sharp')
 
-const JPEG_QUALITY = 40
+const QUALITY = 40
 
 module.exports = (context, req, res) => {
   const imageUrl = req.query.url
   if (!imageUrl.match(/^https?:/i)) return res.status(400).end()
 
-  const transformer = sharp().grayscale().toFormat('webp', {
-    quality: JPEG_QUALITY,
-    compressionLevel: 9,
-    progressive: true
-  })
+  const transformer = sharp().grayscale().toFormat('webp', { quality: QUALITY })
   transformer.on('error', err => console.log(`Error in ${imageUrl}: ${err}`))
   transformer.on('data', processedImage => {
     console.log(`Compressed: ${imageUrl}`)

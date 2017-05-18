ce0c2caecdd8a237101a32ba4486005538d38868
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-05-18 13:01:07 +0900

    Load app CSS after Semantic-UI.

--- extension-react/src/popup.js
+++ extension-react/src/popup.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import App from './popup/App'
-import './popup/index.css'
 import 'semantic-ui-css/semantic.css'
+import './popup/index.css'
 
 ReactDOM.render(<App />, document.getElementById('root'))

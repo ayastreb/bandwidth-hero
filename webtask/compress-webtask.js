9a842c0ea19441c0c617b6e784ae6e91172f5696
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-05-19 15:04:21 +0900

    Return how many bytes were compressed in response header.

--- webtask/compress-webtask.js
+++ webtask/compress-webtask.js
@@ -1,3 +1,4 @@
+'use strict'
 const request = require('request')
 const sharp = require('sharp')
 
@@ -7,15 +8,25 @@ module.exports = (context, req, res) => {
   const imageUrl = req.query.url
   if (!imageUrl.match(/^https?:/i)) return res.status(400).end()
 
+  let originalFileSize = 0
   const transformer = sharp().grayscale().toFormat('webp', { quality: QUALITY })
   transformer.on('error', err => console.log(`Error in ${imageUrl}: ${err}`))
   transformer.on('data', processedImage => {
     console.log(`Compressed: ${imageUrl}`)
     res.writeHead(200, {
       'Content-Type': 'image/webp',
-      'Content-Length': processedImage.length
+      'Content-Length': processedImage.length,
+      'X-Original-Size': originalFileSize,
+      'X-Bytes-Saved': originalFileSize - processedImage.length
     })
   })
 
-  request(imageUrl).pipe(transformer).pipe(res)
+  request
+    .get(imageUrl)
+    .on(
+      'response',
+      response => (originalFileSize = response.headers['content-length'])
+    )
+    .pipe(transformer)
+    .pipe(res)
 }

0605dfe5a3daceae0f2863bce3119ecbdc5d4e92
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-05-24 18:15:36 +0900

    Update default proxy URL.

--- src/defaults.js
+++ src/defaults.js
@@ -6,5 +6,5 @@ export default {
     bytesSaved: 0
   },
   whitelist: [],
-  proxyUrl: 'https://wt-e9c9a7a436fcd9273a7f8890849dae65-0.run.webtask.io/bandwidth-hero-webtask'
+  proxyUrl: 'https://wt-e9c9a7a436fcd9273a7f8890849dae65-0.run.webtask.io/bandwidth-hero-proxy'
 }

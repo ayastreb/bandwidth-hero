ae0b317511a362494eb805cde7ca415e750d6dff
Anatoliy Yastreb <anatoliy.yastreb@gmail.com>
2017-05-24 16:39:19 +0900

    Update Express app.

--- server/server.js
+++ server/server.js
@@ -3,29 +3,36 @@ const express = require('express')
 const request = require('request')
 const sharp = require('sharp')
 
-const QUALITY = parseInt(process.env.JPEG_QUALITY) || 40
-const PORT = process.env.PORT || 5000
+const QUALITY = 40
 
 process.on('uncaughtException', err => console.log(`process error: ${err}`))
 
 const app = express()
-app.listen(PORT, () => console.log(`Listening on ${PORT}`))
-app.get('/compress', compress)
-app.get('/', compress)
-
-function compress(req, res) {
+app.get('/', (req, res) => {
   const imageUrl = req.query.url
   if (!imageUrl.match(/^https?:/i)) return res.status(400).end()
 
+  let originalFileSize = 0
   const transformer = sharp().grayscale().toFormat('webp', { quality: QUALITY })
   transformer.on('error', err => console.log(`Error in ${imageUrl}: ${err}`))
   transformer.on('data', processedImage => {
     console.log(`Compressed: ${imageUrl}`)
     res.writeHead(200, {
       'Content-Type': 'image/webp',
-      'Content-Length': processedImage.length
+      'Content-Length': processedImage.length,
+      'X-Original-Size': originalFileSize,
+      'X-Bytes-Saved': originalFileSize - processedImage.length
     })
   })
 
-  request(imageUrl).pipe(transformer).pipe(res)
-}
+  request
+    .get(imageUrl)
+    .on(
+      'response',
+      response => (originalFileSize = response.headers['content-length'])
+    )
+    .pipe(transformer)
+    .pipe(res)
+})
+
+module.exports = app

d45067132d324099e81802fb465af145fdcf2dfa
Travis Juntara <changhaitravis@gmail.com>
2019-06-02 18:14:00 -0700

    modified patchContentSecurity.test.js to test upgrade-insecure-requests

--- src/background/patchContentSecurity.test.js
+++ src/background/patchContentSecurity.test.js
@@ -8,7 +8,7 @@ it('should return new headers array with patched header', () => {
     },
     {
       name: 'Content-Security-Policy',
-      value: "default-src 'none'; img-src https://google.com; block-all-mixed-content;"
+      value: "default-src 'none'; img-src https://google.com; block-all-mixed-content; Upgrade-Insecure-Requests;"
     }
   ]
   const proxyUrl = 'https://webtask.io/bandwidth-hero'
@@ -19,7 +19,7 @@ it('should return new headers array with patched header', () => {
     },
     {
       name: 'Content-Security-Policy',
-      value: "default-src https://webtask.io 'none'; img-src https://webtask.io https://google.com; block-all-mixed-content;"
+      value: "default-src https://webtask.io 'none'; img-src https://webtask.io https://google.com; block-all-mixed-content; Upgrade-Insecure-Requests;"
     }
   ]
 
@@ -32,14 +32,14 @@ it('should strip out "block-all-mixed-content" CSP flag, if the proxyUrl protoco
     const originalHeaders = [
     {
       name: 'Content-Security-Policy',
-      value: "default-src 'none'; img-src https://google.com; block-all-mixed-content; connect-src foobar.baz"
+      value: "default-src 'none'; img-src https://google.com; block-all-mixed-content; connect-src foobar.baz; upgrade-insecure-requests;"
     }
   ]
   const proxyUrl = 'http://webtask.io/bandwidth-hero'
   const expectedHeaders = [
     {
       name: 'Content-Security-Policy',
-      value: "default-src http://webtask.io 'none'; img-src http://webtask.io https://google.com; ; connect-src http://webtask.io foobar.baz"
+      value: "default-src http://webtask.io 'none'; img-src http://webtask.io https://google.com; ; connect-src http://webtask.io foobar.baz; ;"
     }
   ]
 
